626c3ad05cda0165043da13f808a6a030433844b
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-02-14 09:37:01 +0100

    upd

--- src/main/java/edu/ntnu/idatt2001/lectures/generics/AppComputer.java
+++ src/main/java/edu/ntnu/idatt2001/lectures/generics/AppComputer.java
@@ -6,9 +6,12 @@ public class AppComputer {
 
 		Computer<Integer, Integer> adderInt = (x, y) -> x + y;
 		Computer<Double, Double> adderDouble = (x, y) -> x + y;
-		Computer<String, String> adderStr = (x, y) -> x.concat(y);
 
-		System.out.println(adderStr.compute("Lambda ", "Demo"));
+		Computer<String, String> adderStr1 = (x,y) -> x+y;
+		Computer<String, String> adderStr2 = String::concat;
+
+		System.out.println(adderStr1.compute("Lambda ", "Demo"));
+		System.out.println(adderStr2.compute("Lambda ", "Demo"));
 		System.out.println(adderInt.compute(100, 50));
 		System.out.println(adderDouble.compute(100.5, 50.1));
 	}
